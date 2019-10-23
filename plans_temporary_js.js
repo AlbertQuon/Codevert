29a9c3676fa6cc48043c922a6df19de6061a77d9
Zachary Legros <legroszach3921@gmail.com>
2019-10-23 14:58:14 -0400

    added js for small animation and responsiveness

---
+++ plans_temporary_js.js
@@ -0,0 +1,31 @@
+var premium_package = document.getElementById("premium-package")
+premium_package.addEventListener("mouseover", bv_translate)
+premium_package.addEventListener("mouseout", un_bv_translate)
+
+function bv_translate() {
+    var bv_box = document.getElementById("premium-wrapper")
+    bv_box.style.transform = "translate(0px, 0px)"
+}
+
+
+function un_bv_translate() {
+    var bv_box = document.getElementById("premium-wrapper")
+    bv_box.style.transform = "translate(0px, 55px)"
+}
+
+
+function flexContent() {
+    const mq = window.matchMedia( "(max-width: 1400px)" )
+    var wrapper = document.getElementById("package-containers")
+    var package = document.getElementsByClassName("package")
+
+    if (mq.matches) {
+        wrapper.style.flexDirection = "column"
+        package.style.marginBottom = "65px"
+    } else {
+        wrapper.style.flexDirection = "row"  
+        package.style.marginBottom = "0px"
+    }
+}
+
+window.addEventListener("resize", flexContent)
