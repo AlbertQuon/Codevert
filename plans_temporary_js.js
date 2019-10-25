929758edf2abd9fefa4f24e60f8aeff4f3b52c49
Zachary Legros <legroszach3921@gmail.com>
2019-10-25 09:58:37 -0400

    changed whole design

--- plans_temporary_js.js
+++ plans_temporary_js.js
@@ -1,31 +1,9 @@
-var premium_package = document.getElementById("premium-package")
-premium_package.addEventListener("mouseover", bv_translate)
-premium_package.addEventListener("mouseout", un_bv_translate)
+window.addEventListener("resize", matchHeight)
 
-function bv_translate() {
-    var bv_box = document.getElementById("premium-wrapper")
-    bv_box.style.transform = "translate(0px, 0px)"
+function matchHeight() {
+    var height = document.getElementById("codevert-premium")
+    var getHeight = height.offsetHeight
+    let root = document.documentElement;
+
+    root.style.setProperty('--height', getHeight + 'px')
 }
-
-
-function un_bv_translate() {
-    var bv_box = document.getElementById("premium-wrapper")
-    bv_box.style.transform = "translate(0px, 57px)"
-}
-
-
-/* function flexContent() {
-    const mq = window.matchMedia( "(max-width: 1400px)" )
-    var wrapper = document.getElementById("package-containers")
-    var package = document.getElementsByClassName("package")
-
-    if (mq.matches) {
-        wrapper.style.flexDirection = "column"
-        package.style.marginBottom = "65px"
-    } else {
-        wrapper.style.flexDirection = "row"  
-        package.style.marginBottom = "0px"
-    }
-} */
-
-window.addEventListener("resize", flexContent)
