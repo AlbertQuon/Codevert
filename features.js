8be753c6c7f1c343436fa40a18c96a6841e45f6a
Albert Quon <albertq2001@hotmail.com>
2019-11-14 18:20:39 -0500

    image highlighting

--- features.js
+++ features.js
@@ -1,29 +1,40 @@
 var i = 0;
 var text = "FEATURES";
 var speed = 100;
-
-// FOR TYPING
+var images;
+// FOR PRODUCT TITLE TYPING
 function typeWriter(){
     if(i < text.length) {
         document.getElementById("producttitle").innerHTML += text.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
-}
-
+}  
 // IMAGE HIGHLIGHTING
 function highlight(){
-    document.getElementById("featurePic");
+    this.style.borderColor = "rgba(0, 128, 255, 0.651)";
 }
-
+function reverseHighlight(){
+    this.style.borderColor = "rgba(119, 136, 153, 0.295)";
+}
 
 // FOR FADING IN AND OUT
 $(document).ready(function(){
-    $(window).on('load', function(){
+    $(window).on('load', function(){ // load the window
+        // hide elements for fade in
         $("#productFeature").hide().fadeIn(2000);
         $("#extraFeatTable").hide();
+        // for adding highlight for images
+        images = document.getElementsByClassName("featurePic");
+        for(var j = 0; j < images.length; j++){
+            images[j].addEventListener("mouseout", reverseHighlight)
+            images[j].addEventListener("mouseover", highlight)
+        }
     });
     $("#extraFeatures").mouseover(function(){
         $("#extraFeatTable").fadeIn(1000);
     });
 });
+
+
+ 
